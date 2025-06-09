72cdb73b374ca0394d850ba57b85a732472d2567
digaaonas <59072454+digaaonas@users.noreply.github.com>
2025-06-09 23:02:37 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
 
-async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ chave: "Aqui vem uma mensagem" });
+async function status(requeste, response) {
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnValue = databaseMaxConnResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnValue = databaseOpenedConnResult.rows[0].count;
+
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnValue),
+        opened_connections: databaseOpenedConnValue,
+      },
+    },
+  });
 }
 
 export default status;
